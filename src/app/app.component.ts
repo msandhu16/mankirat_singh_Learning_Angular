f0089f17135b80fbd15a8805cd83ff83158c8ae6
Mankirat Singh <w0857232@myscc.ca>
2024-09-18 19:15:22 -0400

    Assignment 2 Commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Person} from "./models/person";
+
+
 
 @Component({
   selector: 'app-root',
@@ -13,4 +16,51 @@ export class AppComponent {
   age:number = 18;
   goodProgrammer:boolean = true;
   name:string = 'Mankirat';
+
+  person1 : Person={
+    firstName : "Mankirat",
+    lastName : "Singh",
+    age : 18,
+    isProgrammer : true,
+    profession : "student"
+
+}
+
+  person2 : Person={
+    firstName : "Tom",
+    lastName : "Smith",
+    age : 30,
+    profession : "Software Engineer"
+
+  }
+
+  person3 : Person={
+    firstName : "John",
+    lastName : "Doe",
+    age : 45,
+    isProgrammer : true,
+    profession : "Professor"
+
+  }
+
+  person4 : Person={
+    firstName : "Joanna",
+    lastName : "Jones",
+    age : 26,
+    profession : "Architect",
+    isProgrammer: false
+
+  }
+
+  person5 : Person={
+    firstName : "Tom",
+    lastName : "Henderson",
+    age : 23,
+    profession : "Web Developer",
+    isProgrammer: true
+
+  }
+
+
+  person: Person[] = [this.person1 , this.person2 , this.person3 ,this.person4, this.person5 ]
 }
