c2f4360dd4ccbf48ab9b7d0fb609d05b41f98049
Mankirat Singh <w0857232@myscc.ca>
2024-09-18 21:05:45 -0400

    Assignment 2 Commit 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Person} from "./models/person";
+import {NgForOf, NgIf} from "@angular/common";
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
