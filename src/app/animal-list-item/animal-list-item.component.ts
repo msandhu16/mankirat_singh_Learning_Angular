7e12bc05ddd9e83a2dafe3d9557d3b7c6c8d7cc5
Mankirat Singh <w0857232@myscc.ca>
2024-09-28 17:37:42 -0400

    Assignment 3 Error fixed

--- src/app/animal-list-item/animal-list-item.component.ts
+++ src/app/animal-list-item/animal-list-item.component.ts
@@ -1,10 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {AnimalInfo} from "../models/animal-info";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-animal-list-item',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './animal-list-item.component.html',
   styleUrl: './animal-list-item.component.css'
 })
