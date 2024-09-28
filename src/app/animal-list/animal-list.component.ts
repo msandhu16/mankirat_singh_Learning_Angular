28d5f9736dbed13c7a717fd1da95204a58049d7e
Mankirat Singh <w0857232@myscc.ca>
2024-09-28 18:01:41 -0400

    Assignment 3 Commit

--- src/app/animal-list/animal-list.component.ts
+++ src/app/animal-list/animal-list.component.ts
@@ -2,12 +2,14 @@ import {Component, Input} from '@angular/core';
 import {Animals} from "../models/animals";
 import {AnimalInfo} from "../models/animal-info";
 import {AnimalListItemComponent} from "../animal-list-item/animal-list-item.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-animal-list',
   standalone: true,
   imports: [
-    AnimalListItemComponent
+    AnimalListItemComponent,
+    NgForOf
   ],
   templateUrl: './animal-list.component.html',
   styleUrl: './animal-list.component.css'
