ddedb0a1e34eb5ba95116dfa6e650161729be71f
Mankirat Singh <w0857232@myscc.ca>
2024-10-02 18:41:52 -0400

    Assignment 4 Commit 2

--- src/app/animal-list/animal-list.component.ts
+++ src/app/animal-list/animal-list.component.ts
@@ -16,37 +16,5 @@ import {NgClass, NgForOf} from "@angular/common";
   styleUrl: './animal-list.component.css'
 })
 export class AnimalListComponent {
-  animalList: Animals[] = [
-    {
-      name: 'Max',
-      type: 'Dog',
-      color: 'Brown',
-      age: 5
-    },
-    {
-      name: 'Whiskers',
-      type: 'Cat',
-      color: 'White',
-      age: 3
-    },
-    {
-      name: 'Charlie',
-      type: 'Parrot',
-      color: 'Green',
-      age: 2
-    },
-    {
-      name: 'Bella',
-      type: 'Horse',
-      color: 'Black',
-      age: 7
-    }
-  ];
 
-  animalInfo: AnimalInfo[] = [
-    { animal: this.animalList[0], gender: 'Male', weight: 10 },
-    { animal: this.animalList[1], gender: 'Female', weight: 8 },
-    { animal: this.animalList[2], gender: 'Male', weight: 3 },
-    { animal: this.animalList[3], gender: 'Female', weight: 80 }
-  ];
 }
---
+++ src/data/mock_content.ts
@@ -0,0 +1,36 @@
+import {Animals} from "../app/models/animals";
+import {AnimalInfo} from "../app/models/animal-info";
+
+let animalList: Animals[] = [
+  {
+    name: 'Max',
+    type: 'Dog',
+    color: 'Brown',
+    age: 5
+  },
+  {
+    name: 'Whiskers',
+    type: 'Cat',
+    color: 'White',
+    age: 3
+  },
+  {
+    name: 'Charlie',
+    type: 'Parrot',
+    color: 'Green',
+    age: 2
+  },
+  {
+    name: 'Bella',
+    type: 'Horse',
+    color: 'Black',
+    age: 7
+  }
+];
+
+export let animalInfo: AnimalInfo[] = [
+  { animal: animalList[0], gender: 'Male', weight: 10 },
+  { animal: animalList[1], gender: 'Female', weight: 8 },
+  { animal: animalList[2], gender: 'Male', weight: 3 },
+  { animal: animalList[3], gender: 'Female', weight: 80 }
+];
