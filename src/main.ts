dd8b0ec05054b4136c9ae4cc21e38f1791ff765e
msandhu16 <157641868+msandhu16@users.noreply.github.com>
2024-10-07 20:15:02 -0400

    Defined routes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from "@angular/router";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+const routes: Routes = [];
+
+bootstrapApplication(AppComponent,{
+  providers: [provideRouter(routes)]
+}).then(r => console.log('Bootstrap successful'));
+
+
+
+
