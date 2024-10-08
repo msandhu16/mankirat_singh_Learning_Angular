f291d7ea322f75f970a4f77182709e3db5b80d09
msandhu16 <157641868+msandhu16@users.noreply.github.com>
2024-10-08 13:47:54 -0400

    Defined routes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import {provideRouter, Routes} from "@angular/router";
+import {AnimalListComponent} from "./app/animal-list/animal-list.component";
+import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
+import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [{path:'', redirectTo: '/students', pathMatch: 'full'}, //default route
+  { path: 'animals', component: AnimalListComponent },
+  {path:'modify-animal', component: ModifyListItemComponent},
+  {path: '**', component:PageNotFoundComponent}];
 
 bootstrapApplication(AppComponent,{
   providers: [provideRouter(routes)]
